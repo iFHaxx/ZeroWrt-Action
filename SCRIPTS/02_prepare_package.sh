0ff6a05acd19db31c2b6ea901b2dc56618ce4d66
iFHaxx <anzclan@proton.me>
2025-04-20 13:33:40 +0800

    Update 02_prepare_package.sh

--- SCRIPTS/02_prepare_package.sh
+++ SCRIPTS/02_prepare_package.sh
@@ -50,13 +50,13 @@ mkdir -p files/root
 curl -so files/root/.bash_profile https://git.kejizero.online/zhao/files/raw/branch/main/root/.bash_profile
 curl -so files/root/.bashrc https://git.kejizero.online/zhao/files/raw/branch/main/root/.bashrc
 
-# Switch to ImmortalWrt Uboot and Target
-rm -rf target/linux/qualcommax
-cp -rf ../immortalwrt/target/linux/qualcommax target/linux/qualcommax
-rm -rf package/boot/{rkbin,uboot-rockchip,arm-trusted-firmware-rockchip}
-cp -rf ../immortalwrt/package/boot/uboot-rockchip package/boot/uboot-rockchip
-cp -rf ../immortalwrt/package/boot/arm-trusted-firmware-rockchip package/boot/arm-trusted-firmware-rockchip
-sed -i '/REQUIRE_IMAGE_METADATA/d' target/linux/qualcommax/armv8/base-files/lib/upgrade/platform.sh
+# Switch to ImmortalWrt U-Boot and Target (Qualcomm IPQ807x only)
+#rm -rf target/linux/qualcommax
+#cp -rf ../immortalwrt/target/linux/qualcommax target/linux/qualcommax
+#rm -rf package/boot/uboot-ipq807x
+#cp -rf ../immortalwrt/package/boot/uboot-ipq807x package/boot/uboot-ipq807
+#sed -i '/REQUIRE_IMAGE_METADATA/d' target/linux/qualcommax/armv8/base-files/lib/upgrade/platform.sh
+
 
 # Disable Mitigations
 sed -i 's,rootwait,rootwait mitigations=off,g' target/linux/qualcommax/image/default.bootscript
