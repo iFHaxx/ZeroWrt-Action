1112242d7f66981f4eaf6b1ec82728a004c45aeb
iFHaxx <anzclan@proton.me>
2025-04-20 13:03:41 +0800

    Update 02_prepare_package.sh

--- SCRIPTS/02_prepare_package.sh
+++ SCRIPTS/02_prepare_package.sh
@@ -53,7 +53,6 @@ curl -so files/root/.bashrc https://git.kejizero.online/zhao/files/raw/branch/ma
 # Switch to ImmortalWrt Uboot and Target
 rm -rf target/linux/qualcommax
 cp -rf ../immortalwrt/target/linux/qualcommax target/linux/qualcommax
-cp -rf ../OpenWrt-Patch/qualcommax/* ./target/linux/qualcommax/patches-6.6/
 rm -rf package/boot/{rkbin,uboot-rockchip,arm-trusted-firmware-rockchip}
 cp -rf ../immortalwrt/package/boot/uboot-rockchip package/boot/uboot-rockchip
 cp -rf ../immortalwrt/package/boot/arm-trusted-firmware-rockchip package/boot/arm-trusted-firmware-rockchip
@@ -71,11 +70,8 @@ sed -i '3 a\\t\t"order": 50,' feeds/luci/applications/luci-app-ttyd/root/usr/sha
 sed -i 's/procd_set_param stdout 1/procd_set_param stdout 0/g' feeds/packages/utils/ttyd/files/ttyd.init
 sed -i 's/procd_set_param stderr 1/procd_set_param stderr 0/g' feeds/packages/utils/ttyd/files/ttyd.init
 
-# Change default IP
-sed -i "s/192.168.1.1/10.0.0.1/g" package/base-files/files/bin/config_generate
-
 # Change Name
-sed -i 's/OpenWrt/ZeroWrt/' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/DotyWRT/' package/base-files/files/bin/config_generate
 
 # Banner
 cp -rf ../openwrt-package/banner  ./package/base-files/files/etc/banner
@@ -83,68 +79,3 @@ cp -rf ../openwrt-package/banner  ./package/base-files/files/etc/banner
 ### FW4 ###
 rm -rf ./package/network/config/firewall4
 cp -rf ../openwrt_main/package/network/config/firewall4 ./package/network/config/firewall4
-
-# make olddefconfig
-wget -qO - https://raw.githubusercontent.com/oppen321/OpenWrt-Patch/refs/heads/kernel-6.6/kernel/0003-include-kernel-defaults.mk.patch | patch -p1
-
-# LRNG
-cp -rf ../OpenWrt-Patch/lrng/* ./target/linux/generic/hack-6.6/
-echo '
-# CONFIG_RANDOM_DEFAULT_IMPL is not set
-CONFIG_LRNG=y
-CONFIG_LRNG_DEV_IF=y
-# CONFIG_LRNG_IRQ is not set
-CONFIG_LRNG_JENT=y
-CONFIG_LRNG_CPU=y
-# CONFIG_LRNG_SCHED is not set
-CONFIG_LRNG_SELFTEST=y
-# CONFIG_LRNG_SELFTEST_PANIC is not set
-' >>./target/linux/qualcommax/config-6.6
-
-# Module
-cp -rf ../OpenWrt-Patch/kernel/0001-linux-module-video.patch ./ 
-git apply 0001-linux-module-video.patch
-
-# BBR
-cp -rf ../OpenWrt-Patch/bbr3/* ./target/linux/qualcommax/backport-6.6/
-
-# BcmFullCone
-cp -rf ../OpenWrt-Patch/bcmfullcone/* ./target/linux/qualcommax/hack-6.6/
-
-# FW4
-mkdir -p package/network/config/firewall4/patches
-cp -f ../OpenWrt-Patch/firewall/firewall4_patches/*.patch package/network/config/firewall4/patches/
-sed -i 's|$(PROJECT_GIT)/project|https://github.com/openwrt|g' package/network/config/firewall4/Makefile
-
-# libnftnl
-mkdir -p package/libs/libnftnl/patches
-cp -f ../OpenWrt-Patch/firewall/libnftnl/*.patch package/libs/libnftnl/patches/
-
-# nftables
-mkdir -p package/network/utils/nftables/patches
-cp -f ../OpenWrt-Patch/firewall/nftables/*.patch package/network/utils/nftables/patches/
-
-# Shortcut-FE support
-cp -rf ../OpenWrt-Patch/sfe/* ./target/linux/qualcommax/hack-6.6/
-
-# NAT6
-patch -p1 < ../OpenWrt-Patch/firewall/100-openwrt-firewall4-add-custom-nft-command-support.patch
-
-# igc-fix
-cp -rf ../OpenWrt-Patch/igc-fix/* ./target/linux/qualcommax/patches-6.6/
-
-# btf
-cp -rf ../OpenWrt-Patch/btf/* ./target/linux/qualcommax/hack-6.6/
-
-# arm64 model names
-cp -rf ../OpenWrt-Patch/arm/* ./target/linux/qualcommax/hack-6.6/
-
-# cgroupfs-mount
-# Fix unmount hierarchical mount
-pushd feeds/packages
-patch -p1 < ../../../OpenWrt-Patch/pkgs/cgroupfs-mount/0001-fix-cgroupfs-mount.patch
-popd
-# Mount cgroup v2 hierarchy to /sys/fs/cgroup/cgroup2
-mkdir -p feeds/packages/utils/cgroupfs-mount/patches
-cp -rf ../OpenWrt-Patch/pkgs/cgroupfs-mount/900-mount-cgroup-v2-hierarchy-to-sys-fs-cgroup-cgroup2.patch ./feeds/packages/utils/cgroupfs-mount/patches/
-cp -rf ../OpenWrt-Patch
